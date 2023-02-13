ec46d1a3bfbfd252e7d80a2ce8c6bf07a7a04fca
TênMac <caoducmanh2411999@gmail.com>
2023-02-13 18:45:46 +0300

    Carousel Header -byMacMie

--- javascript/header_home.js
+++ javascript/header_home.js
@@ -4,6 +4,14 @@ $(".carousel-slider").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 3000,
     mobileFirst: true
 });
+
+$('.carousel-slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    $(slick.$slides[currentSlide]).removeClass('active');
+  });
+  
+  $('.carousel-slider').on('afterChange', function (event, slick, currentSlide) {
+    $(slick.$slides[currentSlide]).addClass('active');
+  });
