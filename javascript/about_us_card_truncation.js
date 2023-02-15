c9aa032b5252375172087b1b6e8312b245bb8130
TênMac <caoducmanh2411999@gmail.com>
2023-02-15 20:09:16 +0300

    About Us Creating -byMacMie

---
+++ javascript/about_us_card_truncation.js
@@ -0,0 +1,17 @@
+const parentContainer =  document.querySelector('.section-truncation-container');
+
+parentContainer.addEventListener('click', event=>{
+
+    const current = event.target;
+
+    const isReadMoreBtn = current.className.includes('read-more-btn');
+
+    if(!isReadMoreBtn) return;
+
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+    currentText.classList.toggle('read-more-text--show');
+
+    current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+
+})
