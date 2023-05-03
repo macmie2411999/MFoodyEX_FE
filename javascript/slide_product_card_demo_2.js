c427579d2c4c925ad2338e275805354b1e090883
TênMac <caoducmanh2411999@gmail.com>
2023-05-03 19:05:54 +0300

    Slider and Catalog Page -byMacMie

--- javascript/slide_product_card_demo_2.js
+++ javascript/slide_product_card_demo_2.js
@@ -1,9 +1,16 @@
-var splide = new Splide('.splide', {
-    type: 'loop',
-    perPage: 3,
-    rewind: true,
-    autoplay: true,
-    interval: 3000,
-});
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOMContentLoaded triggered');
 
-splide.mount();
+    var splide = new Splide('.splide', {
+        type: 'loop',
+        perPage: 3,
+        rewind: true,
+        autoplay: true,
+        interval: 3000,
+        speed: 2000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    }).mount();
+
+    console.log('Splide initialized');
+});
